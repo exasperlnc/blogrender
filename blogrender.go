82255fa94748ae4d9fedc52039b7a1b68cce7c7d
Logan Cole <emtlogancole@gmail.com>
2024-08-29 15:13:42 -0500

    add template using embed

--- blogrender.go
+++ blogrender.go
@@ -1,6 +1,7 @@
 package blogrender
 
 import (
+	"embed"
 	"html/template"
 	"io"
 )
@@ -9,12 +10,12 @@ type Post struct {
 	Title, Description, Body string
 	Tags 										 []string
 }
-
-const (
-	postTemplate = `<h1>{{.Title}}</h1><p>{{.Description}}</p>Tags: <ul>{{range .Tags}}<li>{{.}}</li>{{end}}</ul>`
+var (
+	//go:embed "templates/*"	
+	postTemplates embed.FS
 )
 func Render(w io.Writer, p Post) error {
-	tmpl, err := template.New("blog").Parse(postTemplate)
+	tmpl, err := template.ParseFS(postTemplates, "templates/*.gohtml")
 	if err != nil {
 		return err
 	}
